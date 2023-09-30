9e8e7daf3416cee56c6a996e6caa69d654a746d8
Adam Skinner <adam@adams-mbp.home>
2023-09-29 21:06:38 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,12 +1,26 @@
+import java.io.*;
 public class Driver {
-	public static void main(String [] args) {
+	public static void main(String [] args) throws IOException {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		double [] c1_co = {6,5};
+		int[] c1_exp = {0, 3};
+		Polynomial p1 = new Polynomial(c1_co, c1_exp);
+		double [] c2_co = {-2, -9};
+		int[] c2_exp = {1, 4};
+		Polynomial p2 = new Polynomial(c2_co, c2_exp);
 		Polynomial s = p1.add(p2);
+		
+		for (double c: s.coeff) {
+			System.out.print(c + ", ");
+		}
+		System.out.println();
+		
+		for (int e: s.exp) {
+			System.out.print(e + ", ");
+		}
+		System.out.println();
+		
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
 		if(s.hasRoot(1)) {
 				System.out.println("1 is a root of s");
@@ -14,5 +28,10 @@ public class Driver {
 		else {
 				System.out.println("1 is not a root of s");
 		}
+		
+		Polynomial m = p1.multiply(p2);
+		System.out.println("m(0.1) = " + m.evaluate(0.1));
+		
+		m.saveToFile("polyout.txt");
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,45 +1,200 @@
+import java.io.*;
+import java.util.*;
 class Polynomial {
 	double[] coeff;
+	int[] exp;
 
+	public Polynomial(File f) throws IOException {
+		
+		Scanner input = new Scanner(f);
+		String poly = input.next();
+		
+		int terms = poly.split("[-+]").length;
+		if (poly.charAt(0) == '-') terms--;
+		coeff = new double[terms];
+		exp = new int[terms];
+		
+		char[] chars = poly.toCharArray();
+		boolean isNegative = false;
+		
+		int index_coeff = 0;
+		int index_exp = 0;
+		int i = 0;
+		while (i < chars.length) {
+			if (chars[i] == '-') {
+				isNegative = true;
+			}
+			
+			if (Character.isDigit(chars[i])) {
+				int j = i;
+				String number = "";
+				while (j < chars.length && chars[j] != 'x' && chars[j] != '+' && chars[j] != '-') {
+					number += chars[j];
+					j++;
+				}
+				
+				if (chars[j] == '+' || chars[j] == '-') {
+					exp[0] = 0;
+					index_exp++;
+				}
+				
+				double num = Double.parseDouble(number);
+				
+				if (isNegative) {
+					coeff[index_coeff] = -1.0 * num;
+					isNegative = false;
+				} else {
+					coeff[index_coeff] = num;
+				}
+				index_coeff++;
+				i = j;
+				continue;
+			}
+			
+			if (chars[i] == 'x') {
+				int j = i+1;
+				String exponent = "";
+				while (j < chars.length && chars[j] != '+' && chars[j] != '-') {
+					exponent += chars[j];
+					j++;
+				}
+				
+				int exponent_num = Integer.parseInt(exponent);
+				exp[index_exp] = exponent_num;
+				index_exp++;
+				i = j;
+				continue;
+			}
+			
+			i++;
+		}
+		
+	}
+	
 	public Polynomial() {
 		coeff = new double[1];
+		exp = new int[1];
 		coeff[0] = 0.0;
+		exp[0] = 0;
 	}
 	
-	public Polynomial(double[] input) {
+	public Polynomial(double[] input, int[] input_exp) {
 		coeff = new double[input.length];
 		coeff = input;
+		exp = new int[input_exp.length];
+		exp = input_exp;
 	}
 	
+	
 	public Polynomial add(Polynomial p) {
-		int len = Math.min(this.coeff.length, p.coeff.length);
-		int max_len = Math.max(this.coeff.length, p.coeff.length);
-		double[] out = new double[max_len];
 		
-		for (int i = 0; i < len; i++) {
-			out[i] = p.coeff[i] + this.coeff[i];
-		}
+		// Determine resultant degree
+		int degree = 0;
 		
-		if (this.coeff.length > p.coeff.length) {
-			for (int i = len; i < max_len; i++) {
-				out[i] = this.coeff[i];
-			}
-		} else if (this.coeff.length < p.coeff.length) {
-			for (int i = len; i < max_len; i++) {
-				out[i] = p.coeff[i];
+		for (int curr_exp: this.exp) {
+			if (curr_exp > degree) {
+				degree = curr_exp;
 			}
 		}
 		
-		Polynomial pout = new Polynomial(out);
+		for (int curr_exp: p.exp) {
+			if (curr_exp > degree) {
+				degree = curr_exp;
+			}
+		}
+		
+		// Add coefficents
+		double[] outWithZeros = new double[degree + 1];
+		for (int i = 0; i < this.coeff.length; i++) {
+			outWithZeros[this.exp[i]] += this.coeff[i];
+		}
+		for (int i = 0; i < p.coeff.length; i++) {
+			outWithZeros[p.exp[i]] += p.coeff[i];
+		}
+		
+		// Determine length of resultant arrays, disregarding any zero coefficients
+		int nonZeroTerms = 0;
+		
+		for (double single_coeff: outWithZeros) {
+			if (single_coeff != 0.0) nonZeroTerms++;
+		}
+		
+		double[] outCoeff = new double[nonZeroTerms];
+		int[] outExp = new int[nonZeroTerms];
+		
+		int out_index = 0;
+		for (int i = 0; i < outWithZeros.length; i++) {
+			if (outWithZeros[i] != 0.0) {
+				outCoeff[out_index] = outWithZeros[i];
+				outExp[out_index] = i;
+				out_index++;
+			}
+		}
+		
+		Polynomial pout = new Polynomial(outCoeff, outExp);
 		return pout;
+	}
+	
+	public Polynomial multiply(Polynomial p) {
+		
+		// Determine degree of resultant
+		int degree1 = 0;
+		for (int curr_exp: this.exp) {
+			if (curr_exp > degree1) {
+				degree1 = curr_exp;
+			}
+		}
+		
+		int degree2 = 0;
+		for (int curr_exp: p.exp) {
+			if (curr_exp > degree2) {
+				degree2 = curr_exp;
+			}
+		}
+		
+		int degree = degree1+degree2;
+		
+		double outCoeffWithZeros[] = new double[degree+1];
+		int outExpWithZeros[] = new int[degree+1];
+		
+		for (int i = 0; i < this.exp.length; i++) {
+			for (int j = 0; j < p.exp.length; j++) {
+				double coeffProd = this.coeff[i] * p.coeff[j];
+				int resExp = this.exp[i] + p.exp[j];
+				
+				outCoeffWithZeros[resExp] += coeffProd;
+				outExpWithZeros[resExp] = resExp;
+			}
+		}
+		
+		// Remove zeros
+		int nonzeros = 0;
+		for (double curr_coeff: outCoeffWithZeros) {
+			if (curr_coeff != 0.0) nonzeros++;
+		}
+		
+		double outCoeff[] = new double[nonzeros];
+		int outExp[] = new int[nonzeros];
+		
+		int index = 0;
+		for (int i = 0; i < outCoeffWithZeros.length; i++) {
+			if (outCoeffWithZeros[i] != 0.0) {
+				outCoeff[index] = outCoeffWithZeros[i];
+				outExp[index] = outExpWithZeros[i];
+				index++;
+			}
+		}
+		
+		Polynomial pout = new Polynomial(outCoeff, outExp);
+		return pout;
+		
 	}
 	
 	public double evaluate(double arg) {
 		double len = (double) this.coeff.length;
 		double result = 0;
-		int index = 0;
 		for (int i = 0; i < len; i++) {
-			result += this.coeff[i] *  Math.pow(arg, i);
+			result += this.coeff[i] *  Math.pow(arg, this.exp[i]);
 		}
 		return result;
 	}
@@ -49,5 +204,16 @@ class Polynomial {
 			return true;
 		}
 		return false;
+	}
+	
+	public void saveToFile(String filename) throws IOException {
+		FileWriter f = new FileWriter(filename, false);
+		for (int i = 0; i < coeff.length; i++) {
+			if (coeff[i] > 0 && i == 0) f.write(Double.toString(coeff[i]));
+			else if (coeff[i] > 0) f.write("+" + Double.toString(coeff[i]));
+			else if (coeff[i] < 0) f.write(Double.toString(coeff[i]));
+			if (i != 0) f.write("x" + Integer.toString(exp[i]));
+		}
+		f.close();
 	}
 }
